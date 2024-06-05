7c62debb6e57fe2d4a6e20d74d38c3084f41aefc
1363v4 <louis-emmanuel.gires@evabssi.com>
2024-06-05 18:05:52 +0200

    dev

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, Response
+from components import home_page
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return Response(home_page())
 
 if __name__ == '__main__':
     app.run()
--- components.py
+++ components.py
@@ -1,1 +1,16 @@
-#h
+import htpy as h
+
+def header():
+    return h.head[
+        h.title["hello world"],
+        h.meta(charset="UTF-8"),
+        h.meta(name="viewport", content="width=device-width, initial-scale=1.0"),
+        h.link(rel="stylesheet", href="/static/css/simple.min.css"),
+        h.script(src="/static/js/htmx.min.js")
+    ]
+
+def home_page():
+    return h.html[
+        header(),
+        "hello world"
+    ]
